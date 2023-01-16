69152110f674f02e03ebdd5c3b44579f7b7d407e
supriya <supriyaarora1990@gmail.com>
2023-01-16 23:03:02 +0530

    slider, footer, testimonials

--- js/app.js
+++ js/app.js
@@ -43,3 +43,19 @@ setTimeout(showSlides, 2000)
 //   slides[slideIndex-1].style.display = "block";
 //   setTimeout(showSlides, 2000); // Change image every 2 seconds
 // }
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 5,
+  spaceBetween: 30,
+  slidesPerGroup: 5,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
