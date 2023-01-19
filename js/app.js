3b6b5a05a04d45c19df91789273612cc482ade19
supriya <supriyaarora1990@gmail.com>
2023-01-19 23:38:42 +0530

    GMB (MAp) Testimonial Stars

--- js/app.js
+++ js/app.js
@@ -87,24 +87,24 @@ var swiper = new Swiper(".mySwiper", {
     // when window width is >= 320px
     320: {
       slidesPerView: 2,
-      spaceBetween: 30,
+      spaceBetween: 10,
       slidesPerGroup: 2
     },
     // when window width is >= 480px
     480: {
       slidesPerView: 2,
-      spaceBetween: 30,
+      spaceBetween: 10,
       slidesPerGroup: 2
     },
     // when window width is >= 640px
     640: {
       slidesPerView: 2,
-      spaceBetween: 30,
+      spaceBetween: 10,
       slidesPerGroup: 2
     },
     768: {
       slidesPerView: 5,
-      spaceBetween: 30,
+      spaceBetween: 10,
       slidesPerGroup: 5,
     }
   }
